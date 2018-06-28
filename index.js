774da67d17026fc2cd83bb1904f7a639293b94bc
victorloo <ila9970@gmail.com>
2018-06-29 01:40:05 +0800

    Integrating Socket.IO

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 var app = require('express')();
 var http = require('http').Server(app); 
-//Express initializes app to be a function handler that you can supply to an HTTP server 
+// Express initializes app to be a function handler that you can supply to an HTTP server 
+var io = require('socket.io')(http);
+// Initialize a new instance of socket.io by passing the http (the HTTP server) object.
 
 app.get('/', function(req, res){
   res.sendfile(__dirname + '/index.html');
 });
 // We define a route handler / that gets called when we hit our website home.
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+// Listen on the connection event for incoming sockets, and log it to the console.
 
 http.listen(3000, function(){
   console.log('listening on *:3000');

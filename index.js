1e682ffb2d4634051dcffc821bfda0f33daa7073
victorloo <ila9970@gmail.com>
2018-06-29 01:44:18 +0800

    Emitting events so that terminal can receive message

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ app.get('/', function(req, res){
 // We define a route handler / that gets called when we hit our website home.
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
   });
 });
 // Listen on the connection event for incoming sockets, and log it to the console.

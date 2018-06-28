b2564f057dd1ff340b529eda85c0e5cbd00fb574
victorloo <ila9970@gmail.com>
2018-06-29 01:29:30 +0800

    Project initialize and finish step about Hello world

---
+++ index.js
@@ -0,0 +1,14 @@
+var app = require('express')();
+var http = require('http').Server(app); 
+//Express initializes app to be a function handler that you can supply to an HTTP server 
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello World</h1>');
+});
+// We define a route handler / that gets called when we hit our website home.
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+// We make the http server listen on port 3000.
+// run node index.js
